4ebc38a0443d6734440eb80da6d5e0063f242ff7
Jacob Fuhrman <jacob.c.fuhrman@gmail.com>
2011-11-23 21:20:16 -0800

    Test commit from Xcode on Jake's mac!

---
+++ GraphicsFinal/GraphicsFinal/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GraphicsFinal
+//
+//  Created by Jacob Fuhrman on 11/23/11.
+//  Copyright 2011 University of Puget Sound. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
